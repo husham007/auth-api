d3396febb666e49b37f9cbb5204484b3f6c6f687
ad.hanif <ad.hanif@outlook.com>
2023-10-26 14:16:28 +0200

    testing route and contoller created

--- app.js
+++ app.js
@@ -20,12 +20,16 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./routes/testingRoutes");
+  app.use("/api/auth/testing", testingRouter);
+}
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Auth server application." });
 });
 
-// import routes from "./routes/auth.routes.js";
 routes(app);
 
 module.exports = app;
--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
