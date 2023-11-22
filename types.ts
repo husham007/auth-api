7349f4b9fd53ea16fd3a2f39a68a2af7afd10e2e
ad.hanif <ad.hanif@outlook.com>
2023-11-22 22:53:35 +0100

    made a backup copy of development branch

---
+++ types.ts
@@ -0,0 +1,11 @@
+import express, { Request } from "express";
+import jwt from "jsonwebtoken";
+
+export interface UserInterface {
+  username: string;
+  email: string;
+  password: string;
+}
+export interface CustomRequest extends Request {
+  user: string | jwt.JwtPayload;
+}
