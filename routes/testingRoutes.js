622c42a6c8447b4a210b167a343407d97ffaadfb
ad.hanif <ad.hanif@outlook.com>
2023-12-03 12:13:58 +0100

    deleted un necessary files

--- routes/testingRoutes.js
+++
@@ -1,6 +0,0 @@
-const testingRouter = require("express").Router();
-const { resetDatabase } = require("../controllers/testing.Controller");
-
-testingRouter.post("/reset", resetDatabase);
-
-module.exports = testingRouter;
--- server.mjs
+++
@@ -1,6 +0,0 @@
-import app from "./app.js";
-import config from "./utils/config.js";
-
-app.listen(config.PORT, () => {
-  console.log(`Auth server is running on port ${config.PORT}.`);
-});
